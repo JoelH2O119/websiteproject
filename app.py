0dd9d7a9504714550b71ec43cbeb68d30fb82131
Matthew Eynon <40650365@live.napier.ac.uk>
2025-11-17 12:26:05 +0000

    hello my g

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/charactercreation")
+def charactercreation():
+    return render_template("charactercreation.html")
+    
+@app.route("/wikipage")
+def wikipage():
+    return render_template("wikipage.html")    
 
 if __name__ == "__main__":
     app.run(debug=True)
